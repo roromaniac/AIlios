a214b917feedeb7362d97d8ce18185d2dbd47c8e
roromaniac <rando4ukraine@gmail.com>
2024-07-12 02:09:41 -0400

    remove the discord cli

---
+++ extract_messages.py
@@ -0,0 +1,40 @@
+# You will need to download the DiscordChatExporter.Cli in order to extract the messages from the Discord server.
+# You can download it from https://github.com/Tyrrrz/DiscordChatExporter/releases
+
+import os
+import subprocess
+import itertools as it
+from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+load_dotenv()
+
+DATE_FORMAT = "%Y-%m-%d"
+BASE_DIR = os.path.join(os.path.dirname(__file__), "knowledge-files")  # Get the directory of the current Python file
+
+# Get the current date
+current_date = datetime.now()
+
+# Generate the last day of the previous 13 months in chronological order
+dates = [(current_date - relativedelta(months=i)).replace(day=1) - relativedelta(days=1) for i in range(12, -1, -1)]
+dates = [date.strftime(DATE_FORMAT) for date in dates]
+
+commands = []
+for after_date, before_date in zip(dates, dates[1:]):
+    output_file = os.path.join(BASE_DIR, f"{datetime.strptime(before_date, DATE_FORMAT).strftime('%B_%Y')}.json")
+    media_dir = os.path.join(BASE_DIR, f"{datetime.strptime(before_date, DATE_FORMAT).strftime('%B_%Y')} MEDIA")
+    commands.append([
+        "DiscordChatExporter.Cli.exe", "export", "-t", os.getenv("DISCORD_SCRAPER_TOKEN"), 
+        "-c", "721205500556869642", "-f", "Json", "-o", output_file, 
+        "--media", "--reuse-media", "--media-dir", media_dir, 
+        "--after", after_date, "--before", before_date
+    ])
+
+def run_command(command):
+    subprocess.run(command, shell=True, cwd=os.path.join(BASE_DIR, "DiscordChatExporter.Cli"))
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    executor.map(run_command, commands)
+
